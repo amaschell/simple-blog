3ca0c63dca1ca9623631adee87169f461e513bb2
Amadeus Schell <amadeus.schell17@gmail.com>
2017-08-18 22:41:06 +0200

    Initial commit.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './css/app.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>This is a simple blog that is under construction.</div>
+    );
+  }
+}
+
+export default App;
