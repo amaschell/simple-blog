e63365cdc2862b773591d1de668e3f626d5bbb20
Amadeus Schell <amadeus.schell17@gmail.com>
2017-10-31 14:29:32 +0100

    Mocking blog posts by using extra JSON file.

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,22 +1,22 @@
 import React from 'react';
 
+import posts from './posts.json';
 import '../Posts/posts.css';
 
 class Posts extends React.Component {
-    render() {
-        // TODO: Use real mocks for this!
-        const posts = [
-            {id: "1", date: "12.12.2012", title: "1. Entry"},
-            {id: "2", date: "12.12.2013", title: "2. Entry"},
-            {id: "3", date: "12.12.2014", title: "3. Entry"},
-            {id: "4", date: "12.12.2015", title: "4. Entry"},
-            {id: "5", date: "12.12.2016", title: "5. Entry"}
-        ];
+    constructor(props) {
+        super(props);
 
+        this.state = {
+            posts: posts
+        };
+    }
+
+    render() {
         return <nav className="posts">
             <ul className="posts__list">
                 {
-                    posts.map(function(post, i) {
+                    this.state.posts.map(function(post, i) {
                         return <div key={post.id}>{post.title}</div>
                     })
                 }
