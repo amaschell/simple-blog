a2a8674abb446f2aa59eb728366ae1e82a8659f9
Amadeus Schell <amadeus.schell17@gmail.com>
2018-02-24 02:13:02 +0100

    Implemented styling and testing for UnknownRoute component.

--- src/components/UnknownRoute/UnknownRoute.js
+++ src/components/UnknownRoute/UnknownRoute.js
@@ -3,9 +3,19 @@ import React from 'react';
 import './unknownRoute.css';
 
 const UnknownRoute = () => (
-    <main>
-        404!
-    </main>
+    <div className="unknownRoute">
+        <span className="unknownRoute__icon">
+                <i className="em em-black_heart"/>
+        </span>
+
+        <span className="unknownRoute__code">
+            404
+        </span>
+
+        <p className="unknownRoute__message">
+            Sorry, this page does not exist...
+        </p>
+    </div>
 );
 
 export default UnknownRoute;
--- src/components/UnknownRoute/UnknownRoute.test.js
+++ src/components/UnknownRoute/UnknownRoute.test.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {shallow} from 'enzyme';
 
 import UnknownRoute from './UnknownRoute';
 
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<UnknownRoute />, div);
+describe('UnknownRoute', () => {
+    let shallowWrapper;
+
+    beforeEach(() => {
+        shallowWrapper = shallow(<UnknownRoute />);
+    });
+
+    test('Renders without crashing.', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(shallowWrapper, div);
+    });
+
+    test('Renders at least the 404 code.', () => {
+        const message = shallowWrapper.find(".unknownRoute");
+        expect(message.text()).toMatch("404");
+    });
 });
