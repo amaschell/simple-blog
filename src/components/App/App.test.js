c4c740488f78da9868beb9da0c833da77b8984fb
Amadeus Schell <amadeus.schell17@gmail.com>
2018-02-24 03:30:44 +0100

    Implemented more tests.

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,9 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, MemoryRouter} from 'react-router-dom';
+import {mount} from 'enzyme';
 
 import App from './App';
+import Header from "../Header/Header";
+import Main from "../Main/Main";
+import Footer from "../Footer/Footer";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+describe('App', () => {
+    let mountedWrapper;
+
+    beforeEach(() => {
+        // We need to wrap the component inside a router here because the App component uses react-router-dom's
+        // BrowserRouter component and needs therefore a router context.
+        mountedWrapper = mount(
+            <MemoryRouter>
+                <App/>
+            </MemoryRouter>
+        );
+    });
+
+    test('Renders without crashing.', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(mountedWrapper, div);
+    });
+
+    test('Should have a Router.', () => {
+        expect(mountedWrapper.find(BrowserRouter)).toHaveLength(1);
+    });
+
+    test('Should have a Header.', () => {
+        expect(mountedWrapper.find(Header)).toHaveLength(1);
+    });
+
+    test('Should have a Main section.', () => {
+        expect(mountedWrapper.find(Main)).toHaveLength(1);
+    });
+
+    test('Should have a Footer.', () => {
+        expect(mountedWrapper.find(Footer)).toHaveLength(1);
+    });
+
+    test('Descendants should have the following order: Header, Main, Footer.', () => {
+        const wrapperDiv = mountedWrapper.find('.app');
+
+        expect(wrapperDiv.children().length).toEqual(3);
+        expect(wrapperDiv.childAt(0).is(Header)).toEqual(true);
+        expect(wrapperDiv.childAt(1).is(Main)).toEqual(true);
+        expect(wrapperDiv.childAt(2).is(Footer)).toEqual(true);
+    });
 });
--- src/components/Main/Main.test.js
+++ src/components/Main/Main.test.js
@@ -1,9 +1,111 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {MemoryRouter, Route, Switch} from 'react-router-dom';
+import {mount, shallow} from 'enzyme';
 
+import About from '../About/About';
+import Contact from "../Contact/Contact";
+import Home from "../Home/Home";
 import Main from './Main';
+import Post from '../Post/Post';
+import Posts from '../Posts/Posts';
+import UnknownRoute from "../UnknownRoute/UnknownRoute";
 
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Main />, div);
+// We need to wrap the component inside a router several times here because the Main component uses react-router-dom's
+// Switch and Route component and needs therefore a router context.
+describe('Main', () => {
+
+    test('Main component renders without crashing.', () => {
+        const div = document.createElement('div');
+        const wrapper = mount(
+            <MemoryRouter>
+                <Main />
+            </MemoryRouter>
+        );
+
+        ReactDOM.render(wrapper, div);
+    });
+
+    test('Should be rendered with as <main> element.', () => {
+        const wrapper = mount(
+            <MemoryRouter>
+                <Main />
+            </MemoryRouter>
+        );
+
+        expect(wrapper.find(Main).childAt(0).type()).toEqual('main');
+    });
+
+    test('Should have a Switch.', () => {
+        const wrapper = mount(
+            <MemoryRouter>
+                <Main />
+            </MemoryRouter>
+        );
+
+        expect(wrapper.find(Switch)).toHaveLength(1);
+    });
+
+    test('Unknown page should redirect to 404.', () => {
+        const wrapper = mount(
+            <MemoryRouter initialEntries={[ '/random' ]}>
+                <Main />
+            </MemoryRouter>
+        );
+
+        expect(wrapper.find(Home)).toHaveLength(0);
+        expect(wrapper.find(UnknownRoute)).toHaveLength(1);
+    });
+
+    test('Should render the home page for /.', () => {
+        const wrapper = mount(
+            <MemoryRouter initialEntries={[ '/' ]}>
+                <Main />
+            </MemoryRouter>
+        );
+
+        expect(wrapper.find(Home)).toHaveLength(1);
+    });
+
+    /*test('Should render the about page for /about.', () => {
+        const wrapper = mount(
+            <MemoryRouter initialEntries={[ '/about' ]}>
+                <Main />
+            </MemoryRouter>
+        );
+
+        expect(wrapper.find(About)).toHaveLength(1);
+    });
+
+    test('Should render the posts page for /posts.', () => {
+        const wrapper = mount(
+            <MemoryRouter initialEntries={[ '/posts' ]}>
+                <Main />
+            </MemoryRouter>
+        );
+
+        expect(wrapper.find(Posts)).toHaveLength(1);
+    });
+
+    test('Should render the post page for /posts/test.', () => {
+        const wrapper = mount(
+            <MemoryRouter initialEntries={[ '/posts/test' ]}>
+                <Main />
+            </MemoryRouter>
+        );
+
+        expect(wrapper.find(Post)).toHaveLength(1);
+    });*/
+
+    test('Should render the contact page for /contact.', () => {
+        const wrapper = mount(
+            <MemoryRouter initialEntries={[ '/contact' ]}>
+                <Main />
+            </MemoryRouter>
+        );
+
+        expect(wrapper.find(Contact)).toHaveLength(1);
+    });
 });
+
+
