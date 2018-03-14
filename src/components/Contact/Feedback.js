5cb08c8ced8d2d006e29d2aea01399c926d7a1aa
Amadeus Schell <amadeus.schell17@gmail.com>
2018-03-14 02:09:17 +0100

    Slight refactoring.

--- src/components/Contact/Feedback.js
+++ src/components/Contact/Feedback.js
@@ -2,21 +2,28 @@ import React from 'react';
 
 import './contact.css';
 
-const Feedback = (props) => {
-    let mainClass;
-
-    if (props.type === "SUCCESS") {
-        mainClass = "contact__feedback--success";
-    } else {
-        mainClass = "contact__feedback--failure";
+class Feedback extends React.Component {
+    constructor(props) {
+        super(props);
     }
 
-    return (
-        <div className={mainClass}>
-            <h3 className='contact__feedbackTitle'>{props.title}</h3>
-            <div className='contact__feedbackMessage'>{props.message}</div>
-        </div>
-    );
-};
+    componentDidMount() {
+        // As this component shows the result of the contact form POST request to the server,
+        // we want the user to directly see this important message by scrolling it into focus.
+        window.scrollTo(0, this.divToFocus.offsetTop);
+    }
+
+    render() {
+        return (
+            <div tabIndex="-1"
+                 className={(this.props.type === "SUCCESS" ? 'contact__feedback--success' : 'contact__feedback--failure')}
+                 ref={(node) => { this.divToFocus = node; }}>
+
+                <h3 className='contact__feedbackTitle'>{this.props.title}</h3>
+                <div className='contact__feedbackMessage'>{this.props.message}</div>
+            </div>
+        );
+    }
+}
 
 export default Feedback;
--- src/components/Contact/Field.js
+++ src/components/Contact/Field.js
@@ -3,31 +3,35 @@ import React from 'react';
 import './contact.css';
 
 const Field = (props) => {
-    const labelStyle = props.isValid ? "contact__formLabel" : "contact__formLabel--notValid";
-
     let inputElement;
 
     if (props.type === 'BIG') {
-        const inputStyle = props.isValid ? "contact__formInput--big" : "contact__formInput--bigAndNotValid";
-        inputElement = <textarea value={props.value} className={inputStyle} rows="6"
+        inputElement = <textarea value={props.value} rows="6"
+                                 className={(props.isValid ?
+                                             "contact__formInput--big" : "contact__formInput--bigAndNotValid")}
                                  onChange={(e) => props.valueChanged(e.target.value)} />;
     } else {
-        const inputStyle = props.isValid ? "contact__formInput--small" : "contact__formInput--smallAndNotValid";
-        inputElement = <input type="text" value={props.value} className={inputStyle}
-                               onChange={(e) => props.valueChanged(e.target.value)} />;
+        inputElement = <input type="text" value={props.value}
+                              className={(props.isValid ?
+                                          "contact__formInput--small" : "contact__formInput--smallAndNotValid")}
+                              onChange={(e) => props.valueChanged(e.target.value)} />;
     }
 
     const errorMessage = props.isValid ?
-          null :
-          <small className="contact__formFieldError">
-              <i className="fa fa-exclamation-triangle"/> {props.errorText}
-          </small>;
+                         null :
+                         <small className="contact__formFieldError">
+                            <i className="fa fa-exclamation-triangle"/> {props.errorText}
+                         </small>;
 
 
     return (
         <div className="contact__formField">
-            <label className={labelStyle}>{props.name}</label>
+            <label className={(props.isValid ? "contact__formLabel" : "contact__formLabel--notValid")}>
+                {props.name}
+            </label>
+
             {inputElement}
+
             {errorMessage}
         </div>
     );
